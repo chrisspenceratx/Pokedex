18b370ebda1f23daa1e74b3b2206a1c4dfb5c865
chrisspenceratx <chrisspenceratx@gmail.com>
2022-04-11 20:19:11 -0500

    1.2 - Initial Commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
-alert('Hello world');
+let pokemonList = [];
 
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+// Bulbasaur stats
+let bulbasaurName = 'Bulbasaur';
+let bulbasaurHeight = 7;
+let bulbasaurType = 'grass';
+
+// Ivysaur stats
+let ivysaurName = 'Ivysaur';
+let ivysaurHeight = 8;
+let ivysaurType = 'poison';
+
+// Venusaur stats
+let venusaurName = 'Venusaur';
+let venusaurHeight = 9;
+let venusaurType = 'quick';
