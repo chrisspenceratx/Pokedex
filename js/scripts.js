fe8689f3c15012b9730ca28ccba3b60c34d1d340
chrisspenceratx <chrisspenceratx@gmail.com>
2022-04-13 08:27:12 -0500

    1.3 - Init Commit

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,16 @@ let pokemonList = [
     {name: 'Ivysaur', height: 8, type: 'poison'},
     {name: 'Venusaur', height: 9, type: 'quick'}
 ];
+
+
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write('Name: ', pokemonList[i].name)
+    document.write('<br>');
+    document.write('Height: ', pokemonList[i].height);
+    document.write('<br>');
+    document.write('<br>');
+}
+
+if (pokemonList[i].height > 8) {
+    document.write('Wow that is big!');
+}
