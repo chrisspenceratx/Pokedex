b3c0dc472173a54537daab2b6b544a566d917cc8
chrisspenceratx <chrisspenceratx@gmail.com>
2022-04-13 16:51:39 -0500

    1.3 - second commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,15 @@
+// Pokemon character specs
 let pokemonList = [
     {name: 'Bulbasaur', height: 7, type: 'grass'},
     {name: 'Ivysaur', height: 8, type: 'poison'},
     {name: 'Venusaur', height: 9, type: 'quick'}
 ];
 
-
-for (let i = 0; i < pokemonList.length; i++) {
-    document.write('Name: ', pokemonList[i].name)
-    document.write('<br>');
-    document.write('Height: ', pokemonList[i].height);
-    document.write('<br>');
-    document.write('<br>');
-}
-
-if (pokemonList[i].height > 8) {
-    document.write('Wow that is big!');
-}
+// Loops for Pokemon names/heights
+for (let i=0; i < pokemonList.length; i++)  {
+    if (pokemonList[i].height >= 8) {
+    document.write(pokemonList[i].name + " (" + "height " + pokemonList[i].height + ") " + "- Wow thats big! <br />");
+    } else {
+    document.write(pokemonList[i].name + " (" + "height " + pokemonList[i].height + ") <br />");
+    }
+    }
