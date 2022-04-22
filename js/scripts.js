490dd6fbc9051380082d45b8fb43d6331d8cb1f0
chrisspenceratx <chrisspenceratx@gmail.com>
2022-04-22 13:47:26 -0500

    1.5 - Javascript Functions (second commit)

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-
 //**This was for PT 1.  My PT 1 forEach loop --
 //  pokemonList.forEach(function(pokemon) {
 //  document.write(pokemon.name + ' is ' + pokemon.height + ' inches long, and ' + pokemon.type + ' type. <br />');
@@ -7,33 +6,36 @@
 
 
 // IIFE
-  let pokemonRepository = (function () {
+let pokemonRepository = (function () {
     let pokemonList = [
             {name: 'Bulbasaur', height: 7, type: 'grass'},
             {name: 'Ivysaur', height: 8, type: 'poison'},
             {name: 'Venusaur', height: 9, type: 'quick'}
         ]
-   
+
 // Public Functions     
         function getAll() {
             return pokemonList;
             }
-    
+
         function add(pokemon) {
         pokemonList.push(pokemon);
+        }
+
+    
+
+        return {
+            getAll: getAll,
+            add: add
+        }   
+    
+    
+         })();
+    
+    
+    // Revised PT2 forEach loop
+    pokemonRepository.getAll().forEach (function(pokemon) {
+        if (pokemon.height >= 8)  {document.write(pokemon.name + ' is ' + pokemon.height + ' inches long, and ' + pokemon.type + ' type. Wow that is big! <br />');
+    }   else if (pokemon.height < 8) {document.write(pokemon.name + 'is ' + pokemon.height + ' inches long, and ' + pokemon.type + ' type. <br />');
     }
-  
-
-    return {
-		getAll: getAll,
-		add: add
-	}   
-
-      
-     })();
-
-     
-// Revised PT2 forEach loop
-pokemonRepository.getAll().forEach (function(pokemon) {
-	document.write(pokemon.name + ' is ' + pokemon.height + ' inches long, and ' + pokemon.type + ' type. <br />');
-});
+}); 
