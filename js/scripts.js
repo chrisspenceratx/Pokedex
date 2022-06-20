9b2ad20de55efd05cc3086070a00516e67611ed4
chrisspenceratx <chrisspenceratx@gmail.com>
2022-06-20 18:57:21 -0500

    added navbar.  added collapse to 'about'

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,25 @@
  */  let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
   
+  // Nav stuff below //
+
+  let search = document.getElementById('pokemon-search');
+  search.addEventListener('input', searchList);
+
+  function searchList() {
+  let searchInput = document.getElementById('pokemon-search').value;
+  searchInput = searchInput.toLowerCase();
+  let listItem = $('li');
+  listItem.each(function () {
+    let item = $(this);
+    let name = item.text();
+    if (name.includes(searchInput)) {
+      item.show();
+    } else {
+      item.hide();
+    }
+  });
+}
   
 //  ---------- ShowModal data listed below --------- //
 
